cedea6ca85b49cb72e6c894728f91cf85cdfbe67
saviooribe <saviooribe@gmail.com>
2022-12-21 11:52:17 -0300

    task header and isEmpty style

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,10 +1,17 @@
 import styles from './Tasks.module.css';
 import { PlusCircle, Trash } from 'phosphor-react';
 import { useState } from 'react';
+import { Clipboard } from '../assets/Tasks';
+
+interface ITasks {
+    id: number;
+    todo: string
+    isCompleted: boolean
+}
 
 export function Tasks() {
     const [checked, setChecked] = useState(true)
-    const [tasks, setTasks] = useState([
+    const [tasks, setTasks] = useState<ITasks[]>([
         {
             id: 1,
             todo: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
@@ -22,17 +29,27 @@ export function Tasks() {
         },
     ])
 
-
     return (
         <div className={styles.tasksContainer}>
             <header className={styles.tasksHeader} >
-                <p>Tarefas criadas <span className="">1</span></p>
-                <p>Concluídas <span className="">1</span></p>
+                <p>Tarefas criadas <span className="">{tasks.length}</span></p>
+                <p>Concluídas <span className="">{tasks.length} de {tasks.length} </span></p>
             </header>
             <div className={styles.tasksList}>
+                {!tasks.length && (
+                    <div className={styles.isEmpty} >
+                        <Clipboard />
+                        <strong>
+                            Você ainda não tem tarefas cadastradas
+                        </strong>
+                        <p>
+                            Crie tarefas e organize seus itens a fazer
+                        </p>
+                    </div>
+
+                )}
                 {tasks.map((task) => {
                     return (
-
                         <div key={task.id} className={styles.task}>
                             <div className={styles.inputAndText}>
                                 <input className={styles.taskInput} onChange={() => setChecked(!checked)} checked={task.isCompleted} type="checkbox" />
